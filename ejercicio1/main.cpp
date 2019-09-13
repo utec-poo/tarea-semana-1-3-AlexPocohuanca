e7efde01f9331097d7704118900472e30be80193
AlexPocohuanca <55201207+AlexPocohuanca@users.noreply.github.com>
2019-09-13 15:56:49 -0500

    Update main.cpp

--- ejercicio1/main.cpp
+++ ejercicio1/main.cpp
@@ -1,17 +1,23 @@
+
 #include <iostream>
 #include "Tipos.h"
+#include <iomanip>
+
 using namespace std;
 
+typedef int entero;
+
 int main() {
+    entero base=0, altura=0;
+    cout << "\ningrese la base:";
+    cin >> base;
+    cout << "\ningrese la altura:";
+    cin >> altura;
 
-entero base=0, entero altura=0;
-cout << "\ningrese la base:";
-cin >> base;
-cout << "\ningrese la altura:";
-cin >> altura;
+    float area;
 
+    area=base*altura/2.0;
 
-// ToDO
-
-  return 0;
+    cout<<fixed<<setprecision(4)<<area;
+    return 0;
 }
