8e8340d5f36e7da6811e8840e1129cd12355ad99
AlexPocohuanca <55201207+AlexPocohuanca@users.noreply.github.com>
2019-09-16 23:10:37 -0500

    Update main.cpp

--- ejercicio3/main.cpp
+++ ejercicio3/main.cpp
@@ -6,13 +6,23 @@ int main() {
 
 entero numero;
 caracter crt;
-
+string color;
 cout << "ingrese el caracter:";
 cin >> crt;
 cout << "\ningrese el numero:";
 cin >> numero;
 
-// ToDO
-// implemente el algoritmo.
+if (crt=="a"||crt=="c"||crt=="e"||crt=="g")
+	if (numero%2!=0)
+		color = "black"
+
+	else
+		color = "white"
+else
+	if (numero%2==0)
+		color = "black"
+	else
+		color = "white"
   return 0;
+  cout<<color;
 }
