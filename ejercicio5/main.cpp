513999f4d0b3bb23a98344ee47bcbbdc2538db31
AlexPocohuanca <55201207+AlexPocohuanca@users.noreply.github.com>
2019-09-16 23:27:02 -0500

    Update main.cpp

--- ejercicio5/main.cpp
+++ ejercicio5/main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include "Tipos.h"
+#include <vector>
 using namespace std;
 
 int main() {
 
 entero numero;
 caracter crt;
-
-cout << "ingrese el caracter:";
-cin >> crt;
-cout << "\ningrese el numero:";
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
+vector<int> listaa={};
+do{
+  entero aux;
+cout<<"Ingresa un numero";
+  listaaa.push_back(aux);
+}while(aux!=0);
+  
+  
+  
+  
+  
+  cout<<"Los numeros ingresados son: "<<listaa.size()-1<<endl;
   return 0;
 }
+
