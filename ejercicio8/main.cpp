ce320d0bcd02c6a12d5e1a193aacb0877da26825
AlexPocohuanca <55201207+AlexPocohuanca@users.noreply.github.com>
2019-09-17 23:35:24 -0500

    Update main.cpp

--- ejercicio8/main.cpp
+++ ejercicio8/main.cpp
@@ -5,11 +5,37 @@ using namespace std;
 int main() {
 
 entero numero;
+  do{
+     cout<<"Ingresa un numero: "<<endl;
+     cin >> numero;
+  }while(numero>30 || numero<1);
+  for (int c=0;c<numero;c++){
+   if(c==0){
+     cout<<"* "<<endl;
+   }
+   if(c==numero-1){
+     for(int f=0;f<numero;f++){
+       cout<<"*";
+     }
+   }
+   else{
+     for(int g=0;g<=c;g++){
+       if(g==0){
+         cout<<"* ";
+       }
+       if(g==c){
+         cout<<"* "<<endl;
+       }
+       else{
+         cout<<" ";
+       }
 
-cout << "\ningrese el numero:";
-cin >> numero;
 
-// ToDO
-// implemente el algoritmo.
+     }
+
+
+   }
+  }
+
   return 0;
 }
