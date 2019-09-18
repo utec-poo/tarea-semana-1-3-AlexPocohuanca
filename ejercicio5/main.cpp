3d6d95c51a1b679dca1bcc74175810fc3040c85d
AlexPocohuanca <55201207+AlexPocohuanca@users.noreply.github.com>
2019-09-17 21:42:11 -0500

    Update main.cpp

--- ejercicio5/main.cpp
+++ ejercicio5/main.cpp
@@ -6,18 +6,29 @@ using namespace std;
 int main() {
 
 entero numero;
-caracter crt;
 vector<int> listaa={};
-do{
   entero aux;
-cout<<"Ingresa un numero";
-  listaaa.push_back(aux);
+do{
+  
+  cout<<"Ingresa un numero";
+  cin>>aux;
+  listaa.push_back(aux);
 }while(aux!=0);
+  entero pares=0;
+  entero impares=0;
+  
+
+  for (int c;c<listaa.size()-1;c++){
+   if(listaa[c]%2==0)
+     pares++;
+   else
+     impares++;
+    
+  }
   
   
-  
-  
-  
+  cout<<"La cantidad de pares es: "<<pares<<endl;
+  cout<<"La cantidad de impares es: "<<impares<<endl;
   cout<<"Los numeros ingresados son: "<<listaa.size()-1<<endl;
   return 0;
 }
