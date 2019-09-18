1d3f9af8ea16ba266969b24508b09ef92660170b
AlexPocohuanca <55201207+AlexPocohuanca@users.noreply.github.com>
2019-09-17 23:45:17 -0500

    Update main.cpp

--- ejercicio9/main.cpp
+++ ejercicio9/main.cpp
@@ -3,7 +3,7 @@
 #include "MyFunc.h"
 using namespace std;
 int main() {
-    l_enteros segundos;
+   l_enteros segundos;
     enteros horas, min, seg;
 
     do {
@@ -11,6 +11,11 @@ int main() {
         cin >> segundos;
     } while (segundos <1);
 
-    // ToDO
+    horas=segundos/3600;
+    segundos=segundos%3600;
+    min=segundos/60;
+    segundos=segundos%60;
+    seg=segundos;
+    cout<<horas<<" "<<" "<<min<<" "<<seg<<endl;
     return 0;
 }
